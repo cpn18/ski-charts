41c7faeb6db1c1c6e9f32d09a1ae7ae6d4655542
Jonathan Miner <cp_n18@yahoo.com>
2023-12-21 21:38:32 -0500

    Added Lane Change

---
+++ web/lanechange.js
@@ -0,0 +1,71 @@
+// This is Javascript
+
+var d_of_c = 120 / 6;  // Complete turn = 6 segments
+var l_of_a = 25;      // Each arc is 50 pixels
+var s_p = 55;         // skier's pivot point
+
+// Path is required
+path = [
+	// starting point
+	[0,0,'black','black',90,10,20,20,15,s_p,15,s_p],
+];
+
+left = [
+	// length
+	// heading (positive = right footer, negative = left footer),
+	// right color
+	// left color
+	// right pressure
+	// left pressure
+	// right stance
+	// left stance
+	// right ski pivot angle
+	// right ski pivot point (0-100)
+	// left ski pivot angle
+	// left ski pivot point (0-100)
+	
+
+	// out of the fall line
+	[l_of_a, d_of_c,'blue','gray',90,10,20,20,15,s_p,15,s_p],
+	// bottom of the turn
+	[2*l_of_a-20,2*d_of_c,'red','gray',40,60,20,20,0,s_p,0,s_p],
+	// top of the turn	
+	[2*l_of_a+20, -2*d_of_c, 'gray','green', 10, 90,20,20,-15,s_p,-15,s_p],
+	// into the fall line (specify 90/10 at the fall line)
+	[l_of_a, -1*d_of_c, 'gray', 'blue', 10, 90,20,20,-15,s_p,-15,s_p]
+];
+
+med_left = [
+	// out of the fall line
+	[l_of_a, d_of_c,'blue','gray',90,10,20,20,15,s_p,15,s_p],
+	// bottom of the turn
+	[10*l_of_a-20,3*d_of_c,'red','gray',40,60,20,20,0,s_p,0,s_p],
+	// top of the turn	
+	[4*l_of_a+20, -3*d_of_c, 'gray','green', 10, 90,20,20,-15,s_p,-15,s_p],
+	// into the fall line (specify 90/10 at the fall line)
+	[l_of_a, -1*d_of_c, 'gray', 'blue', 10, 90,20,20,-15,s_p,-15,s_p]
+];
+
+right = [
+	// out of the fall line
+	[l_of_a, -1*d_of_c, 'gray', 'blue', 10, 90,20,20,-15,s_p,-15,s_p],
+	// bottom of the turn
+	[2*l_of_a-20, -2*d_of_c, 'gray', 'red', 60, 40,20,20,0,s_p,0,s_p],
+	// top of the turn	
+	[2*l_of_a+20, 2*d_of_c, 'green','grey', 90, 10,20,20,15,s_p,15,s_p],
+	// into the fall line (specify 90/10 at the fall line)
+	[l_of_a, 1*d_of_c, 'blue', 'grey', 90, 10,20,20,15,s_p,15,s_p]
+];
+
+med_right = [
+	// out of the fall line
+	[l_of_a, -1*d_of_c, 'gray', 'blue', 10, 90,20,20,-15,s_p,-15,s_p],
+	// bottom of the turn
+	[10*l_of_a-20, -3*d_of_c, 'gray', 'red', 60, 40,20,20,0,s_p,0,s_p],
+	// top of the turn	
+	[4*l_of_a+20, 3*d_of_c, 'green','grey', 90, 10,20,20,15,s_p,15,s_p],
+	// into the fall line (specify 90/10 at the fall line)
+	[l_of_a, 1*d_of_c, 'blue', 'grey', 90, 10,20,20,15,s_p,15,s_p]
+];
+
+path = path.concat(left).concat(right).concat(med_left).concat(right).concat(left).concat(med_right);
