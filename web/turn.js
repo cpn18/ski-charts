99c1191105d1d1d63ba89d03e9893bdce881d0cc
Jonathan Miner <cp_n18@yahoo.com>
2025-03-21 05:58:39 -0400

    Update turn example

--- web/turn.js
+++ web/turn.js
@@ -1,42 +1,60 @@
 // This is Javascript
+turnClass = class {
+	constructor() {
+                this.max_width = 8 // Pixels
 
-var d_of_c = 90 / 6;  // Complete turn = 6 segments
-var l_of_a = 50;      // Each arc is 50 pixels
-var s_p = 55;         // skier's pivot point
+                this.center_width = 1 // Pixels
+                this.center_color = 'black'
 
-// Path is required
-path = [
-	// length
-	// heading (positive = right footer, negative = left footer),
-	// right color
-	// left color
-	// right pressure
-	// left pressure
-	// right stance
-	// left stance
-	// right ski pivot angle
-	// right ski pivot point (0-100)
-	// left ski pivot angle
-	// left ski pivot point (0-100)
-	
-	// starting point
-	[0,0,'black','black',90,10,20,20,15,s_p,15,s_p],
+                this.ski_color = 'purple' // Rossi 3G
+                this.ski_length = 100 // Pixels
+                this.ski_width = 1 // Pixels
+                this.ski_mod = 30 // Pixels, how often to draw
 
-	// out of the fall line
-	[l_of_a, d_of_c,'blue','gray',90,10,20,20,15,s_p,15,s_p],
+                this.initial_position = [200, 0]
+        }
 
-	// bottom of the turn
-	[2*l_of_a-20,2*d_of_c,'red','gray',40,60,20,20,0,s_p,0,s_p],
+        // Generate and return the path
+        getPath() {
 
-	// top of the turn	
-	[2*l_of_a+20, -2*d_of_c, 'gray','green', 10, 90,20,20,-15,s_p,-15,s_p],
+		var degreeOfCurve = 90 / 6;  // Complete turn = 6 segments
+		var arcLength = 50;      // Each arc is 50 pixels
+		var pivotPoint = 55;         // skier's pivot point
 
-	// into the fall line (specify 90/10 at the fall line)
-	[l_of_a, -1*d_of_c, 'gray', 'blue', 10, 90,20,20,-15,s_p,-15,s_p],
+		return [
+			// length
+			// heading (positive = right footer, negative = left footer),
+			// right color
+			// left color
+			// right pressure
+			// left pressure
+			// right stance
+			// left stance
+			// right ski pivot angle
+			// right ski pivot point (0-100)
+			// left ski pivot angle
+			// left ski pivot point (0-100)
 
-	// out of the fall line
-	[l_of_a, -1*d_of_c, 'gray', 'blue', 10, 90,20,20,-15,s_p,-15,s_p],
+			// starting point
+			[0,0,'black','black',90,10,20,20,15,pivotPoint,15,pivotPoint],
 
-	// end of the turn
-	[2*l_of_a-20, -2*d_of_c, 'gray', 'red', 60, 40,20,20,0,s_p,0,s_p],
-];
+			// out of the fall line
+			[arcLength, degreeOfCurve,'blue','gray',90,10,20,20,15,pivotPoint,15,pivotPoint],
+
+			// bottom of the turn
+			[2*arcLength-20,2*degreeOfCurve,'red','gray',40,60,20,20,0,pivotPoint,0,pivotPoint],
+
+			// top of the turn
+			[2*arcLength+20, -2*degreeOfCurve, 'gray','green', 10, 90,20,20,-15,pivotPoint,-15,pivotPoint],
+
+			// into the fall line (specify 90/10 at the fall line)
+			[arcLength, -1*degreeOfCurve, 'gray', 'blue', 10, 90,20,20,-15,pivotPoint,-15,pivotPoint],
+
+			// out of the fall line
+			[arcLength, -1*degreeOfCurve, 'gray', 'blue', 10, 90,20,20,-15,pivotPoint,-15,pivotPoint],
+
+			// end of the turn
+			[2*arcLength-20, -2*degreeOfCurve, 'gray', 'red', 60, 40,20,20,0,pivotPoint,0,pivotPoint],
+		];
+	}
+}
