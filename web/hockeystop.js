f15993c15436f64e25fd8b26bbc7ec506da8a470
Jonathan Miner <cp_n18@yahoo.com>
2025-03-21 06:25:45 -0400

    Update hockey stop

--- web/hockeystop.js
+++ web/hockeystop.js
@@ -1,31 +1,49 @@
 // This is Javascript
+turnClass = class {
+        constructor() {
+                this.max_width = 8 // Pixels
 
-var s_p = 55;         // skier's pivot point
+                this.center_width = 1 // Pixels
+                this.center_color = 'black'
 
-// Path is required!
-path = [
-	// length
-	// heading (positive = right footer, negative = left footer),
-	// right color
-	// left color
-	// right pressure
-	// left pressure
-	// right stance
-	// left stance
-	// right ski pivot angle 
-	// right ski pivot point (0-100)
-	// left ski pivot angle
-	// left ski pivot point (0-100)
+                this.ski_color = 'purple' // Rossi 3G
+                this.ski_length = 100 // Pixels
+                this.ski_width = 1 // Pixels
+                this.ski_mod = 30 // Pixels, how often to draw
+
+                this.initial_position = [200, 0]
+        }
+
+        // Generate and return the path
+        getPath() {
+		var skiPivot = 55;         // skier's pivot point
+
+		// Path is required!
+		return [
+			// length
+			// heading (positive = right footer, negative = left footer),
+			// right color
+			// left color
+			// right pressure
+			// left pressure
+			// right stance
+			// left stance
+			// right ski pivot angle 
+			// right ski pivot point (0-100)
+			// left ski pivot angle
+			// left ski pivot point (0-100)
 	
-	// define a starting point (note zero length)
-	[0,0,'black','black',50,50,20,20,0,s_p,0,s_p],
+			// define a starting point (note zero length)
+			[0,0,'black','black',50,50,20,20,0,skiPivot,0,skiPivot],
 
-	// straight run, even pressure
-	[100,0,'black','black',50,50,20,20,0,s_p,0,s_p],
+			// straight run, even pressure
+			[100,0,'black','black',50,50,20,20,0,skiPivot,0,skiPivot],
 
-	// straight run, but skis end pivoted 45 degrees
-	[100,0,'black','black',50,50,20,20,45,s_p,45,s_p],
+			// straight run, but skis end pivoted 45 degrees
+			[100,0,'black','black',50,50,20,20,45,skiPivot,45,skiPivot],
 
-	// short turn, pressure on outside ski, skier turns 45 degrees
-	[50,45,'black','black',90,10,20,20,45,s_p,45,s_p],
+			// short turn, pressure on outside ski, skier turns 45 degrees
+			[50,45,'black','black',90,10,20,20,45,skiPivot,45,skiPivot],
 ];
+	}
+}
